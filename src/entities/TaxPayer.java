a944e61ba0b3d66b76b470cf921f7bdae04d93cd
Iago Lima <iagoclima22@gmail.com>
2026-08-31 11:52:49 -0300

    Add TaxPayer class

---
+++ src/entities/TaxPayer.java
@@ -0,0 +1,95 @@
+package entities;
+
+public class TaxPayer {
+    private Double salaryIncome;
+    private Double servicesIncome;
+    private Double capitalIncome;
+    private Double healthSpending;
+    private Double educationSpending;
+
+    public TaxPayer() {
+    }
+
+    public TaxPayer(Double salaryIncome, Double servicesIncome, Double capitalIncome, Double healthSpending, Double educationSpending) {
+        this.salaryIncome = salaryIncome;
+        this.servicesIncome = servicesIncome;
+        this.capitalIncome = capitalIncome;
+        this.healthSpending = healthSpending;
+        this.educationSpending = educationSpending;
+    }
+
+    public Double getSalaryIncome() {
+        return salaryIncome;
+    }
+
+    public void setSalaryIncome(Double salaryIncome) {
+        this.salaryIncome = salaryIncome;
+    }
+
+    public Double getServicesIncome() {
+        return servicesIncome;
+    }
+
+    public void setServicesIncome(Double servicesIncome) {
+        this.servicesIncome = servicesIncome;
+    }
+
+    public Double getCapitalIncome() {
+        return capitalIncome;
+    }
+
+    public void setCapitalIncome(Double capitalIncome) {
+        this.capitalIncome = capitalIncome;
+    }
+
+    public Double getHealthSpending() {
+        return healthSpending;
+    }
+
+    public void setHealthSpending(Double healthSpending) {
+        this.healthSpending = healthSpending;
+    }
+
+    public Double getEducationSpending() {
+        return educationSpending;
+    }
+
+    public void setEducationSpending(Double educationSpending) {
+        this.educationSpending = educationSpending;
+    }
+
+    public double salaryTax() {
+        if (salaryIncome < 3000.00) {
+            return 0;
+        } else if (salaryIncome < 5000.00) {
+            return salaryIncome * 0.10;
+        } else {
+            return salaryIncome * 0.20;
+        }
+    }
+
+    public double servicesTax() {
+        return servicesIncome * 0.15;
+    }
+
+    public double capitalTax() {
+        return capitalIncome * 0.20;
+    }
+
+    public double grossTax() {
+        return salaryTax() + servicesTax() + capitalTax();
+    }
+
+    public double taxRebate() {
+        double rebateTaxes = healthSpending + educationSpending;
+        if (rebateTaxes >= 0.30 * grossTax()) {
+            return 0.30 * grossTax();
+        } else {
+            return rebateTaxes;
+        }
+    }
+
+    public double netTax() {
+        return grossTax() - taxRebate();
+    }
+}
